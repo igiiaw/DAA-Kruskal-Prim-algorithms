0e6faffd287ac67d0d88d93bac7b1b19be2ee5cc
igiiaw <zhnamantai@gamail.com>
2025-10-26 18:09:13 +0500

    optimize UnionFind by adding path compression and union by rank

--- src/main/java/com/atur/UnionFind.java
+++ src/main/java/com/atur/UnionFind.java
@@ -2,17 +2,30 @@ package com.atur;
 
 public class UnionFind {
     private int[] parent;
+    private int[] rank;
+    public long ops;
     public UnionFind(int n) {
         parent = new int[n];
+        rank = new int[n];
+        ops = 0;
         for (int i = 0; i < n; i++) parent[i] = i;
     }
     public int find(int x) {
-        while (parent[x] != x) x = parent[x];
-        return x;
+        ops++;
+        if (parent[x] != x) parent[x] = find(parent[x]);
+        return parent[x];
     }
-    public void union(int a, int b) {
+    public boolean union(int a, int b) {
         int ra = find(a);
         int rb = find(b);
-        parent[ra] = rb;
+        ops++;
+        if (ra == rb) return false;
+        if (rank[ra] < rank[rb]) parent[ra] = rb;
+        else if (rank[rb] < rank[ra]) parent[rb] = ra;
+        else {
+            parent[rb] = ra;
+            rank[ra]++;
+        }
+        return true;
     }
 }
